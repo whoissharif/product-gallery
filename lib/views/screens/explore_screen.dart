0e44371d0a71119335024ec11c1c477fa8ec5a66
whoissharif <sharif.csebubt@gmail.com>
2022-04-02 23:33:06 +0600

    grid implemented

--- lib/views/screens/explore_screen.dart
+++ lib/views/screens/explore_screen.dart
@@ -1,28 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:product_gallery/controllers/product_controller.dart';
+import 'package:product_gallery/models/product_model.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class ExploreScreen extends StatelessWidget {
   ExploreScreen({Key? key}) : super(key: key);
   final ProductController productController = Get.put(ProductController());
   @override
   Widget build(BuildContext context) {
-    return Obx(
-      () {
-        if (productController.isLoading.value) {
-          return const Center(child: CircularProgressIndicator());
-        } else {
-          return ListView.builder(
-            itemCount: productController.productModel.products.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(productController.productModel.products[index].title
-                    .toString()),
-              );
-            },
-          );
-        }
-      },
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Obx(
+        () {
+          if (productController.isLoading.value) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            List<Product> products = productController.productModel.products;
+            return AlignedGridView.count(
+              crossAxisCount: 2,
+              itemCount: products.length,
+              crossAxisSpacing: 5,
+              mainAxisSpacing: 5,
+              itemBuilder: (context, index) {
+                return ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.network(
+                    products[index].thumbnail,
+                    fit: BoxFit.cover,
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ),
     );
   }
 }
